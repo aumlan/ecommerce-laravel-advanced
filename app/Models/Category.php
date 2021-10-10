dd93d0079897150f2710ce8d2b4f3000f5807841
unknown <aumlan0120@gmail.com>
2021-10-10 21:18:21 +0600

    category seeder added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category){
+            $category->slug = Str::of($category->name)->slug('-');
+        });
+    }
 
     public function parent_category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
